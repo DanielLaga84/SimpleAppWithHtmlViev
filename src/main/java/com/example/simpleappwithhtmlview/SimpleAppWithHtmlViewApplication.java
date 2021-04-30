c695357b9ee6356b4954fbc44bd077ef01fbaa67
daniellaga84@gmail.com <Erkano84!!>
2021-04-30 11:47:43 +0200

    Initial commit

---
+++ src/main/java/com/example/simpleappwithhtmlview/SimpleAppWithHtmlViewApplication.java
@@ -0,0 +1,13 @@
+package com.example.simpleappwithhtmlview;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleAppWithHtmlViewApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SimpleAppWithHtmlViewApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/simpleappwithhtmlview/SimpleAppWithHtmlViewApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.simpleappwithhtmlview;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimpleAppWithHtmlViewApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
